7aa8c606dffe979125924deb127792aa38441a1b
Neil Allwood <neilallwood@icloud.com>
2022-05-25 18:38:43 +0100

    First path operation created

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ app = FastAPI()
 # --- Create a base route, also called a path operation:
 # Note app.get = a GET request.
 @app.get("/")
+# --- Create a function for the path operation:
+# Note: async is optional. Only use async if the application (or the function) doesn't need to
+# communicate with anything else, such as a database. Typically, you would not use this.
 async def root():
-    return {"message": "Hello World!"}
+    return {"greeting": "Hello World!"}
+
+# 40:45
