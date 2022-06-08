e4556f023b975383ba9046e3e2955d3b7cf80f44
Neil Allwood <neilallwood@icloud.com>
2022-06-08 19:17:51 +0100

    Added info for routes for Swagger Docs.

--- app/main.py
+++ app/main.py
@@ -16,7 +16,18 @@ models.Base.metadata.create_all(bind = engine)
 
 
 # --- Create an instance of FastAPI
-app = FastAPI()
+app = FastAPI(
+    # --- These details will be shown on the API documentation:
+    title = "Blog Post API Reference",
+    description ="This API is used for interacting with a database containing blog posts and more.",
+    version = "1.0.1",
+    servers=[
+        {
+            "url": "http://localhost:8000",
+            "description": "Development Server"
+        }
+        ]
+)
 
 
 # --- Setup the connection to the database:
@@ -65,6 +76,7 @@ while connection_successful is False:
 # --- Reference the files that contain the routes / path operations for the application:
 app.include_router(post.router)
 app.include_router(user.router)
+
 
 # --- Create a base route, also called a path operation:
 # --- Note app.get = a GET method. "/" is the URL path.
