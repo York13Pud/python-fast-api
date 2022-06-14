6090fb4fafe871d98c1061d69bd49b7ac4664f21
Neil Allwood <neilallwood@icloud.com>
2022-06-14 13:31:48 +0100

    Further tidy up of main.py.

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
             "description": "Development Server"
         }
         ]
-)
+    )
 
 
 # --- Reference the files that contain the routes / path operations for the application:
@@ -30,18 +30,6 @@ app.include_router(auth.router)
 app.include_router(post.router)
 app.include_router(root.router)
 app.include_router(user.router)
-
-
-# --- Create a base route, also called a path operation:
-# --- Note app.get = a GET method. "/" is the URL path.
-# @app.get("/")
-
-# --- Create a function for the path operation:
-# --- Note: async is optional. Only use async if the application (or the function) doesn't need to
-# --- communicate with anything else, such as a database. Typically, you would not use this.
-
-# async def root():
-#     return {"greeting": "This is not the endpoint you are looking for!"}
 
 
 
