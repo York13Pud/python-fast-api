8c400a46b8802e62ebbec3c55d4dae0def3f221b
Neil Allwood <neilallwood@icloud.com>
2022-06-23 13:49:52 +0100

    Updated test_root to test for http status 200.

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,17 +1,30 @@
 # --- Import the required modules:
-from fastapi.testclient import TestClient
 from app.main import app
+from fastapi.testclient import TestClient
+import pytest
 
 
 # --- Create the client to interact with the app:
 client = TestClient(app)
 
+# --- Make a call to the root URL:
+res = client.get("/")
+
+def test_root_status_code():
+    """Status code must be: 200"""
+    res_status_code = res.status_code
+    expected_status_code = 200
+    assert res_status_code == expected_status_code, \
+           f"The returned status code is: {res_status_code}. It must be {expected_status_code}."
 
 # --- Define a function that will test the root url of the app:
-def test_root():
-    # --- Make a call to the root URL:
-    res = client.get("/")
-    # --- Print out the contents of the "greeting" key:
-    print(res.json().get("greeting"))
+def test_root_greeting():
+    """Greeting text must be: 
+       This is not the endpoint you are looking for!"""
+    
     # --- Test to see if the response matches what we expect:
-    assert res.json().get("greeting") == "This is not the endpoint you are looking for!"
+    res_greeting = res.json().get("greeting")
+    expected_greeting = "This is not the endpoint you are looking for!"
+    
+    assert res_greeting ==  expected_greeting, \
+           f"The returned greeting is: {res_greeting}.\nIt must be: {expected_greeting}."
