ef25c24647efdf6965e0020228693ec809e411a8
Neil Allwood <neilallwood@icloud.com>
2022-06-23 13:52:53 +0100

    Updated test_root with additional spacing.

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,21 +1,25 @@
 # --- Import the required modules:
 from app.main import app
 from fastapi.testclient import TestClient
-import pytest
 
 
 # --- Create the client to interact with the app:
 client = TestClient(app)
 
+
 # --- Make a call to the root URL:
 res = client.get("/")
 
+
 def test_root_status_code():
     """Status code must be: 200"""
+    
     res_status_code = res.status_code
     expected_status_code = 200
+    
     assert res_status_code == expected_status_code, \
            f"The returned status code is: {res_status_code}. It must be {expected_status_code}."
+
 
 # --- Define a function that will test the root url of the app:
 def test_root_greeting():
